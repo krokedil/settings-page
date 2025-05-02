b91e2d4e6b4498a86aa9c81e013254c6858839d9
anyakro <anya@krokedil.se>
2025-05-02 11:48:19 +0200

    Add content instead of description to addons

--- src/Addons.php
+++ src/Addons.php
@@ -122,9 +122,16 @@ class Addons {
 		wp_enqueue_script( 'plugin-install' );
 		add_thickbox(); // Required for the plugin installer to work.
 
-		$addons = $this->addons['items'];
+		$addons  = $this->addons['items'];
+		$content = $this->addons['content'];
 		?>
 		<div class="krokedil_addons">
+		<div class="krokedil_addons__info">
+				<?php foreach ( $content as $item ) : ?>
+					<?php echo wp_kses_post( $this->print_content( $item ) ); ?>
+				<?php endforeach; ?>
+			</div>
+
 			<?php // translators: %s is the plugin name. ?>
 			<div class='krokedil_addons__cards'>
 				<?php foreach ( $addons as $addon ) : ?>
@@ -264,5 +271,55 @@ class Addons {
 			<?php echo wp_kses_post( self::get_link( $link ) ); ?>
 		</span>
 		<?php
+	}
+
+	/**
+	 * Print the content.
+	 *
+	 * @param array $item The item to print.
+	 * @param bool  $ignore_p_tag Whether to ignore the p tag.
+	 *
+	 * @return string
+	 */
+	public function print_content( $item, $ignore_p_tag = false ) {
+		$type = $item['type'];
+
+		$text = '';
+
+		switch ( $type ) {
+			case 'text':
+				$text = self::get_text( $item );
+				break;
+			case 'link':
+				$text = self::get_link( $item );
+				break;
+			case 'link_text':
+				$text = self::get_link_text( $item );
+				break;
+			case 'list':
+				$list_items = $item['items'];
+				$list       = '<ul class="krokedil_settings__list">';
+				foreach ( $list_items as $list_item ) {
+					$list .= '<li>';
+					$list .= $this->print_content( $list_item, true );
+					$list .= '</li>';
+				}
+				$list .= '</ul>';
+
+				$text = $list;
+				break;
+			case 'spacer':
+				$ignore_p_tag = true;
+				$text         = '<div class="krokedil_settings__spacer"></div>';
+				break;
+			default:
+				return '';
+		}
+
+		if ( $ignore_p_tag ) {
+			return $text;
+		}
+
+		return '<p>' . $text . '</p>';
 	}
 }
